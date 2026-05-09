a3a0e69d09a49ad7ae983c90993b9cab9c8687ec
Matthieu Gagné <mathdaman@gmail.com>
2026-05-08 21:10:04 -0400

    feat: restructure objects.tsx as collection-of-images tileset

--- maps/objects.tsx
+++ maps/objects.tsx
@@ -1,6 +1,26 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.8" tiledversion="1.8.2" name="objects" tilewidth="16" tileheight="16" tilecount="3" columns="3">
- <image source="../assets/objects/desk_placeholder.png" width="48" height="16"/>
- <tile id="0" type="wall"/>
- <tile id="1" type="wall"/>
+<tileset version="1.8" tiledversion="1.8.2" name="objects" tilewidth="16" tileheight="16" tilecount="2" columns="0" objectalignment="topleft">
+ <tile id="0" type="instance">
+  <properties>
+   <property name="blocks_movement" type="bool" value="true"/>
+   <property name="examine_text" value=""/>
+   <property name="object_name" value="Desk"/>
+   <property name="res_path" type="file" value="res://scenes/world/WorldObject.tscn"/>
+   <property name="sprite_texture" type="file" value="res://assets/objects/desk_placeholder.png"/>
+   <property name="tile_cols" type="int" value="3"/>
+   <property name="tile_rows" type="int" value="1"/>
+  </properties>
+  <image source="../assets/objects/desk_placeholder.png" width="48" height="16"/>
+ </tile>
+ <tile id="1" type="instance">
+  <properties>
+   <property name="blocks_movement" type="bool" value="true"/>
+   <property name="object_name" value="Iris"/>
+   <property name="res_path" type="file" value="res://scenes/world/NPC.tscn"/>
+   <property name="sprite_texture" type="file" value="res://assets/sprites/characters/iris.png"/>
+   <property name="tile_cols" type="int" value="1"/>
+   <property name="tile_rows" type="int" value="1"/>
+  </properties>
+  <image source="../assets/sprites/characters/iris.png" width="16" height="24"/>
+ </tile>
 </tileset>
