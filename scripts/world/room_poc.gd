9d1670f90a3c4ad2a70c6c2c2e673c390e2252e6
Matthieu Gagné <mathdaman@gmail.com>
2026-05-17 22:48:13 -0400

    feat: add battle_background export to RoomPOC

--- scripts/world/room_poc.gd
+++ scripts/world/room_poc.gd
@@ -1,5 +1,7 @@
 extends Node2D
 
+@export var battle_background: String = "alley"
+
 
 func _ready() -> void:
-	AudioManager.play_music("res://assets/audio/music/NoirCafe.ogg")
+    AudioManager.play_music("res://assets/audio/music/NoirCafe.ogg")
