1ad592eb7abe25dcbdc5183495f9e0d35de5d510
MATTHIEU GAGNÉ <mathdaman@gmail.com>
2026-06-02 19:19:02 -0400

    fix: use is_action_just_pressed for debug_toggle to prevent rapid toggling

--- scripts/autoload/debug_overlay.gd
+++ scripts/autoload/debug_overlay.gd
@@ -46,7 +46,7 @@ func _toggle() -> void:
 
 
 func _unhandled_input(event: InputEvent) -> void:
-	if event.is_action_pressed("debug_toggle"):
+	if event.is_action_just_pressed("debug_toggle"):
 		_toggle()
 
 
