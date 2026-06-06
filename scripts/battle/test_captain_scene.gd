8477ce001d464e642f874b7fad7add1193df3a22
MATTHIEU GAGNÉ <mathdaman@gmail.com>
2026-06-05 22:56:38 -0400

    fix: add local CAPTAIN_RES/ENFORCER_RES constants to debug scenes after BattleScene refactor

--- scripts/battle/test_captain_scene.gd
+++ scripts/battle/test_captain_scene.gd
@@ -1,5 +1,7 @@
 extends BattleScene
 
+const CAPTAIN_RES := "res://characters/enemies/block_captain.tres"
+
 func _spawn_enemies() -> void:
 	var captain: Combatant = load(CAPTAIN_RES).duplicate()
 	captain.reset_runtime_state()
--- scripts/battle/test_enforcer_scene.gd
+++ scripts/battle/test_enforcer_scene.gd
@@ -1,5 +1,7 @@
 extends BattleScene
 
+const ENFORCER_RES := "res://characters/enemies/territory_enforcer.tres"
+
 func _spawn_enemies() -> void:
 	var enforcer: Combatant = load(ENFORCER_RES).duplicate()
 	enforcer.reset_runtime_state()
